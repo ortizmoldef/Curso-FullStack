7513d76952b89f21f9e9d539236642d61a4cd3c4
ortizmoldef <ortizmoldef@alumnos.cei.es>
2025-03-10 12:27:22 +0100

    Update ejercicio2.js

--- dia21/ejercicio2.js
+++ dia21/ejercicio2.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 console.log('Mensaje escrito en el archivo.');
 
-const readStream = fs.createReadStream('./archivo2.txt');
+const readStream = fs.createReadStream('./archivo.txt');
 
 readStream.on('data', chunk => {
   console.log('Contenido del archivo:');
